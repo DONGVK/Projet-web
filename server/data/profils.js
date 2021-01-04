c9596e20855802009dab4b85c345de76e3a08278
WilfriedPonnou <75972109+WilfriedPonnou@users.noreply.github.com>
2021-01-04 09:16:39 +0100

    ajout de mon profil

--- server/data/profils.js
+++ server/data/profils.js
@@ -7,6 +7,15 @@ const profils = [
       experience: []
     },
   ]
+const profils1 = [
+  {
+    id: 3,
+    firstname: 'Wilfried',
+    lastname: 'PONNOU',
+    skills: [{name: 'HTML/CSS', percent: 50}, {name: 'Javascript', percent: 50}],
+    experience: []
+  },
+]
   
   module.exports = profils
   
